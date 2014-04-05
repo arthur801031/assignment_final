de8fe3772c6d91b8ef6bab1baaee30457a3b8be5
Steve Volocyk <sevenex@gmail.com>
2014-04-05 14:22:19 -0500

    add test framework for user and assignments

--- spec/requests/assignments_spec.rb
+++ spec/requests/assignments_spec.rb
@@ -4,8 +4,33 @@ describe "Assignments" do
   describe "GET /assignments" do 
     
     it "should have the content 'assignments'" do
-      visit '/assignments/index'
+      visit '/assignments'
       expect(page).to have_content('assignments')
     end  
+
+    it "should have the title 'Assignments'" do
+      visit '/assignments'
+      expect(page).to have_title('Assignments')
+    end  
+
+    it "should have the title 'New assignment'" do
+      visit '/assignments/new'
+      expect(page).to have_title('New assignment')
+    end 
+    
+    it "should have the content 'Title'" do
+      visit '/assignments/new'
+      expect(page).to have_content('Title')
+    end 
+
+    it "should have the content 'Description'" do
+      visit '/assignments/new'
+      expect(page).to have_content('Description')
+    end 
+
+    it "should have the content 'Duedate'" do
+      visit '/assignments/new'
+      expect(page).to have_content('Duedate')
+    end 
   end
 end
